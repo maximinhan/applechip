4c49de7d6fe2fc5ed3ae3fc386e1fad72c59388d
maximinhan <maximinhan@gmail.com>
2014-04-29 01:29:06 +0900

    프로젝트 환경설정

---
+++ src/test/java/com/applechip/apps/firstTest.java
@@ -0,0 +1,15 @@
+package com.applechip.apps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class firstTest {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
