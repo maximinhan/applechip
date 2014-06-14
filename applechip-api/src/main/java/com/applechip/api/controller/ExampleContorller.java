e40ed6579e6606af86c946042f17f2bc4aecf746
yawn(한지우) <yawn@coupang.com>
2014-06-14 23:21:17 +0900

    applechip-api 생성, gradle 구조 변경

---
+++ applechip-api/src/main/java/com/applechip/api/controller/ExampleContorller.java
@@ -0,0 +1,11 @@
+package com.applechip.api.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yawn
+ * Date: 2014. 6. 14.
+ * Time: 오후 11:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExampleContorller {
+}
