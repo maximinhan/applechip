f074918c2df1e0245d53571dee74b3a76ba1a071
kwyi <kwyi0110@gmail.com>
2014-09-28 13:33:43 +0900

    config.class 제거 eclipse 디폴트 out폴더 변경

--- applechip-web/src/main/java/com/applechip/web/configurer/initializer/WebDispatcherServletInitializer.java
+++ applechip-web/src/main/java/com/applechip/web/configurer/initializer/WebDispatcherServletInitializer.java
@@ -5,15 +5,14 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import com.applechip.core.CoreConfig;
-import com.applechip.web.WebConfig;
 import com.applechip.web.configurer.WebWebMvcConfigurer;
 
 public class WebDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { CoreConfig.class, WebConfig.class };
+		// return new Class[] { CoreConfig.class, WebConfig.class };
+		return null;
 	}
 
 	@Override
