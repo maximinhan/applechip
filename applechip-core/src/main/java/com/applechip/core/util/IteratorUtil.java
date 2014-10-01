467b0b9aed8593d27133c03b15466006e9cf9bf3
kwyi <kwyi0110@gmail.com>
2014-10-01 19:55:52 +0900

    util

--- applechip-core/src/main/java/com/applechip/core/util/IteratorUtil.java
+++ applechip-core/src/main/java/com/applechip/core/util/IteratorUtil.java
@@ -2,6 +2,7 @@ package com.applechip.core.util;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.commons.collections.IteratorUtils;
 
@@ -11,5 +12,10 @@ public class IteratorUtil extends IteratorUtils {
 
 	public static <E> ArrayList<E> newArrayList(Iterator<? extends E> elements) {
 		return Lists.newArrayList(elements);
+	}
+
+	public static <E> Set<E> union(Set<E> set1, Set<E> set2) {
+		set1.addAll(set2);
+		return set1;
 	}
 }
