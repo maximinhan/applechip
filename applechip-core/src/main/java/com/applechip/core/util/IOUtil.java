d4dfe4d5e4a2fd3b772cb353fbab0a10f93b460a
kwyi <kwyi0110@gmail.com>
2014-10-12 20:08:10 +0900

    gradle 빌드 기반 안드로이드 프로젝트 추가

--- applechip-core/src/main/java/com/applechip/core/util/IOUtil.java
+++ applechip-core/src/main/java/com/applechip/core/util/IOUtil.java
@@ -1,6 +1,10 @@
 package com.applechip.core.util;
 
 import java.io.Closeable;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 
 import org.apache.commons.io.IOUtils;
 
@@ -8,7 +12,15 @@ public class IOUtil extends IOUtils {
 
   public static void closeQuietly(Closeable[] closeables) {
     for (Closeable closeable : closeables) {
-      closeQuietly(closeable);
+      if (closeable instanceof OutputStream) {
+        closeQuietly((OutputStream) closeable);
+      } else if (closeable instanceof InputStream) {
+        closeQuietly((OutputStream) closeable);
+      } else if (closeable instanceof Writer) {
+        closeQuietly((OutputStream) closeable);
+      } else if (closeable instanceof Reader) {
+        closeQuietly((OutputStream) closeable);
+      }
     }
   }
 }
