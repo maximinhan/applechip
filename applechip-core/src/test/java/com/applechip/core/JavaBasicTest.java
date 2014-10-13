d09fa3c0356db52846998b0b6db5fbc364f478a0
kwyi <kwyi0110@gmail.com>
2014-10-14 08:54:33 +0900

    concurrent test

--- applechip-core/src/test/java/com/applechip/core/JavaBasicTest.java
+++ applechip-core/src/test/java/com/applechip/core/JavaBasicTest.java
@@ -3,8 +3,11 @@ package com.applechip.core;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.junit.Test;
 
@@ -28,5 +31,41 @@ public class JavaBasicTest {
     map.putIfAbsent("", "");
     Map<String, String> m = Collections.synchronizedMap(new HashMap<String, String>());
     m.put("", "");
+  }
+
+  @Test
+  public void test1() {
+    // List<String> myList = new ArrayList<String>();
+    List<String> myList = new CopyOnWriteArrayList<String>();
+
+    myList.add("1");
+    myList.add("2");
+    myList.add("3");
+    myList.add("4");
+    myList.add("5");
+
+    Iterator<String> it = myList.iterator();
+    while (it.hasNext()) {
+      String value = it.next();
+      System.out.println("List Value:" + value);
+      if (value.equals("3"))
+        myList.remove(value);
+    }
+
+    // Map<String,String> myMap = new ConcurrentHashMap<String,String>();
+    Map<String, String> myMap = new HashMap<String, String>();
+    myMap.put("1", "1");
+    myMap.put("2", "2");
+    myMap.put("3", "3");
+
+    Iterator<String> it1 = myMap.keySet().iterator();
+    while (it1.hasNext()) {
+      String key = it1.next();
+      System.out.println("Map Value:" + myMap.get(key));
+      if (key.equals("2")) {
+        myMap.put("1", "4");
+        myMap.put("4", "4");
+      }
+    }
   }
 }
